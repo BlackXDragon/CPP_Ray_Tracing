54aab16009cea10c609b5da78c036b11f0d454e7
BlackXDragon <malolan98@gmail.com>
2022-01-19 01:53:34 +0000

    Add crt.h

--- include/crt.h
+++ include/crt.h
@@ -1,1 +1,14 @@
+#include <vector3d.h>
+#include <color.h>
+#include <ray.h>
+#include <material.h>
+#include <image.h>
+#include <objects/sphere.h>
+#include <lights/light.h>
 #include <read_config.h>
+
+#if SPEEDUP==2
+	#include <renderer_cuda.h>
+#else
+	#include <renderer.h>
+#endif
